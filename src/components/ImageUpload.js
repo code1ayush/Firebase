9926fcb8e3b902deab77814fd97890b13bc0734d
ayushkumar <ayushverma8900@gmail.com>
2023-09-08 03:25:53 +0530

    minor changes

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,9 +1,22 @@
 import { useState } from "react";
-import { storage } from "./firebase";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { Link } from "react-router-dom";
 
 function ImageUpload() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyArET7qz9SoNd99Q_oXP4_E9WlsdEQN6js",
+    authDomain: "imagestore-61d7d.firebaseapp.com",
+    projectId: "imagestore-61d7d",
+    storageBucket: "imagestore-61d7d.appspot.com",
+    messagingSenderId: "1555043301",
+    appId: "1:1555043301:web:8739a77c451d54f4fbd50c",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const storage = getStorage(app);
   const [imgUrl, setImgUrl] = useState(null);
   const [progresspercent, setProgresspercent] = useState(0);
 
